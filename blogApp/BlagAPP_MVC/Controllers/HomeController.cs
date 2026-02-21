68cd218a1d1c94e424d5bba6e4d886fbcbbe00e5
Zoom7122 <v.lyubchencko@gmail.com>
2026-02-21 20:02:17 +0300

    Начало добавления MVC

---
+++ blogApp/BlagAPP_MVC/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using BlogAPP_BLL.Intarface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlagAPP_MVC.Controllers
+{
+    public class EntaranceController : Controller
+    {
+        private readonly ILoginService _loginService;
+
+        public EntaranceController(ILoginService loginService) 
+        {
+            _loginService = loginService;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
